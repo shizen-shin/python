8393d16c304cbc7f042133ff1fb0f5eac220b97e
yuta-miyasaka38 <hlaupcpky38@gmail.com>
2020-02-06 10:17:31 +0900

    create webdriver file

---
+++ selenium/webdriver.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+browser = webdriver.Chrome('chromedriver.exe')
+browser.get('https://www.google.com/search?q=Google+chrome&rlz=1C1SQJL_jaJP784JP784&oq=Google+chrome&aqs=chrome..69i57j69i64l2j69i60l2.415j0j1&sourceid=chrome&ie=UTF-8')
