e9586d10be578822a4cba2e2e523b81bea484a43
yuta-miyasaka38 <hlaupcpky38@gmail.com>
2020-02-11 10:51:05 +0900

    second commit

--- select-extract-days.py
+++ select-extract-days.py
@@ -6,6 +6,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div(children=[
     html.H1(
         children='Stock price scrayping app',
